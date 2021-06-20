68dbae26d81187a2f7555c37d4c4020f8a3e731f
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-20 13:46:20 -0700

    Initial Commit

---
+++ secure-field/secure_fieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  secure_fieldApp.swift
+//  secure-field
+//
+//  Created by user on 6/20/21.
+//
+
+import SwiftUI
+
+@main
+struct secure_fieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
